22aa26dd6b562ee9b305b399b9b02af44fedbc40
Vignesh Jeyaraj <vignesh.jeyaraj@nike.com>
2018-11-01 07:35:32 -0700

    Fixing the isues with the test

--- tests/functional/pages/SignUpPage.js
+++ tests/functional/pages/SignUpPage.js
@@ -1,8 +1,14 @@
-import { Selector } from 'testcafe';
+import { Selector, t } from 'testcafe';
 
 export default class SignUpPage {
     constructor() {
         this.txtFirstName = Selector(".userdata-firstname").find('input')
         this.txtLastName = Selector(".userdata-lastname").find('input')
+    }
+
+    async setSignUpDetails() {
+        await t
+            .typeText(this.txtFirstName, "ICON")
+            .typeText(this.txtFirstName, "SuperUser")
     }
 }
